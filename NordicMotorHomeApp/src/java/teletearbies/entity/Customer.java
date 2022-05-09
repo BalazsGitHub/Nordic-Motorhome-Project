111f9addb20fc20b9da9f4f497721b5c323a017a
Mubíbic <90600712+mubibic@users.noreply.github.com>
2022-05-09 15:22:26 +0200

    Started on the customer entity and created a onne-to-one relationship with booking

--- NordicMotorHomeApp/src/java/teletearbies/entity/Customer.java
+++ NordicMotorHomeApp/src/java/teletearbies/entity/Customer.java
@@ -1,11 +1,31 @@
 package teletearbies.entity;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
 public class Customer {
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false,unique = false,length = 255, name = "full_name")
     private String fullName;
+
+    @Column(nullable = false, unique = false, length = 255, name = "phone_number")
     private String phoneNumber;
+
+    @Column(nullable = false,unique = false, length = 255, name = "cardNumber")
     private String cardNumber;
+
+    @Column(nullable = false, unique = false, length = 255, name = "address")
     private String address;
+
+    @OneToOne(mappedBy = "customerId")
+    private Booking booking;
+
 
     public Customer(String fullName, String phoneNumber, String cardNumber, String address) {
         this.fullName = fullName;
