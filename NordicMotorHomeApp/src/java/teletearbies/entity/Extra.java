231a3ec7cc257b0eed94f3e0c1ebcfd052f01251
Mubíbic <90600712+mubibic@users.noreply.github.com>
2022-05-09 15:27:41 +0200

    Started on the Extra entity and created a onne-to-one relationship with BookedExtras

--- NordicMotorHomeApp/src/java/teletearbies/entity/Extra.java
+++ NordicMotorHomeApp/src/java/teletearbies/entity/Extra.java
@@ -1,8 +1,24 @@
 package teletearbies.entity;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "extras")
 public class Extra {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Integer id;
+
+   @Column(nullable = false, unique = false, length = 255, name = "name")
    private String name;
+
+   @Column(nullable = false, unique = false, length = 10, name = "price")
    private double price;
+
+   @OneToMany
+   private List<BookedExtras> bookedExtras;
 
    public Extra(){}
 
