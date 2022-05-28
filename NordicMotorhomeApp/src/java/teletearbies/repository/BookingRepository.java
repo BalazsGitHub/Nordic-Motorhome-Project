9d507c4e17772d2bbffb6864812f5cd617ccc996
bacelarfc <fernandacbacelar@gmail.com>
2022-05-28 16:16:57 +0200

    made some comments on the repo

--- NordicMotorhomeApp/src/java/teletearbies/repository/BookingRepository.java
+++ NordicMotorhomeApp/src/java/teletearbies/repository/BookingRepository.java
@@ -4,8 +4,16 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import teletearbies.entity.Booking;
 
-@Repository
-public interface BookingRepository extends CrudRepository<Booking, Integer> {
-    Long countById(Integer id);
 
+//DAO: Data Access Object: The object responsible for CRUD operations. It is basically an object that provides an interface to perform all database operations.
+
+@Repository
+//It is the actual implementation of DAO
+//which simplify all the database operations.
+//We extend the CrudRepository Interface which provides general CRUD functionalities such as retrieve/search/save/delete.
+
+public interface BookingRepository extends CrudRepository<Booking, Integer> {
+
+    //since interface methods are public, static, final by default, there's no need to use 'public' before the data type
+    Long countById(Integer id);
 }
