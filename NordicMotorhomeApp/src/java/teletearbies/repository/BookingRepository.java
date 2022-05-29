e72a962477d408b41858b8253b6f4898591be824
bacelarfc <fernandacbacelar@gmail.com>
2022-05-29 11:32:13 +0200

    comments on the entity

--- NordicMotorhomeApp/src/java/teletearbies/repository/BookingRepository.java
+++ NordicMotorhomeApp/src/java/teletearbies/repository/BookingRepository.java
@@ -8,7 +8,7 @@ import teletearbies.entity.Booking;
 //DAO: Data Access Object: The object responsible for CRUD operations. It is basically an object that provides an interface to perform all database operations.
 
 @Repository
-//It is the actual implementation of DAO
+//@Repository It is the actual implementation of DAO
 //which simplify all the database operations.
 //We extend the CrudRepository Interface which provides general CRUD functionalities such as retrieve/search/save/delete.
 
